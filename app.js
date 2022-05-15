7b28009bb1e823494a87bfc77d569edb46e71bcf
behilsamuel05@gmail.com <behilalemu@icloud.com>
2022-05-15 10:26:12 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const PI = 3.14;
+
+// What is the difference between var and let?
+//The let keyword creates a block-scoped variable and can be reassigned but not var. var can get  reassigned,redeclared and hoist to the top of the page .  
+// What is the difference between var and const?
+//The const keyword creates a block-scoped variable but not var, const doesnot get redeclared or get modified 
+// What is the difference between let and const?
+// you can modifty the values on let but not const 
+// What is hoisting?
+//the interpreter appears to move the declaration of functions, variables or classes to the top of their scope, prior to execution of the code. Only does this.
